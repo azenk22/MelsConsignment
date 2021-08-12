1f905af657a7e5ba4660f672e4e8886128bcf982
Alex Zenk <azenk@css.edu>
2021-08-11 22:47:24 -0500

    Commit

--- MelsConsignment/SearchForm.Designer.cs
+++ MelsConsignment/SearchForm.Designer.cs
@@ -28,56 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SearchBTN = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TypeCMB = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FirstNameTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.LastNameTB = new System.Windows.Forms.TextBox();
-            this.LastNameLBL = new System.Windows.Forms.Label();
+            this.SearchBTN = new System.Windows.Forms.Button();
+            this.GunsDGV = new System.Windows.Forms.DataGridView();
+            this.Make = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Model = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GunsDGV)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.TypeCMB);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.FirstNameTB);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.LastNameTB);
+            this.splitContainer1.Panel1.Controls.Add(this.SearchBTN);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.GunsDGV);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Type:";
+            // 
+            // TypeCMB
+            // 
+            this.TypeCMB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TypeCMB.FormattingEnabled = true;
+            this.TypeCMB.Items.AddRange(new object[] {
+            "Pistol",
+            "Revolver",
+            "Rifle",
+            "Shotgun",
+            "Other"});
+            this.TypeCMB.Location = new System.Drawing.Point(96, 192);
+            this.TypeCMB.Name = "TypeCMB";
+            this.TypeCMB.Size = new System.Drawing.Size(121, 21);
+            this.TypeCMB.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "First Name:";
+            // 
+            // FirstNameTB
+            // 
+            this.FirstNameTB.Location = new System.Drawing.Point(91, 98);
+            this.FirstNameTB.Name = "FirstNameTB";
+            this.FirstNameTB.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameTB.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Last Name:";
+            // 
+            // LastNameTB
+            // 
+            this.LastNameTB.Location = new System.Drawing.Point(91, 73);
+            this.LastNameTB.Name = "LastNameTB";
+            this.LastNameTB.Size = new System.Drawing.Size(100, 20);
+            this.LastNameTB.TabIndex = 16;
             // 
             // SearchBTN
             // 
-            this.SearchBTN.Location = new System.Drawing.Point(72, 125);
+            this.SearchBTN.Location = new System.Drawing.Point(116, 218);
             this.SearchBTN.Name = "SearchBTN";
             this.SearchBTN.Size = new System.Drawing.Size(75, 23);
-            this.SearchBTN.TabIndex = 0;
+            this.SearchBTN.TabIndex = 15;
             this.SearchBTN.Text = "Search";
             this.SearchBTN.UseVisualStyleBackColor = true;
             this.SearchBTN.Click += new System.EventHandler(this.SearchBTN_Click);
             // 
-            // LastNameTB
+            // GunsDGV
             // 
-            this.LastNameTB.Location = new System.Drawing.Point(47, 45);
-            this.LastNameTB.Name = "LastNameTB";
-            this.LastNameTB.Size = new System.Drawing.Size(100, 20);
-            this.LastNameTB.TabIndex = 1;
+            this.GunsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GunsDGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Make,
+            this.Model});
+            this.GunsDGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GunsDGV.Location = new System.Drawing.Point(0, 0);
+            this.GunsDGV.Name = "GunsDGV";
+            this.GunsDGV.Size = new System.Drawing.Size(530, 450);
+            this.GunsDGV.TabIndex = 0;
             // 
-            // LastNameLBL
+            // Make
             // 
-            this.LastNameLBL.AutoSize = true;
-            this.LastNameLBL.Location = new System.Drawing.Point(395, 54);
-            this.LastNameLBL.Name = "LastNameLBL";
-            this.LastNameLBL.Size = new System.Drawing.Size(35, 13);
-            this.LastNameLBL.TabIndex = 2;
-            this.LastNameLBL.Text = "label1";
+            this.Make.HeaderText = "Make";
+            this.Make.Name = "Make";
+            // 
+            // Model
+            // 
+            this.Model.HeaderText = "Model";
+            this.Model.Name = "Model";
             // 
             // SearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.LastNameLBL);
-            this.Controls.Add(this.LastNameTB);
-            this.Controls.Add(this.SearchBTN);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "SearchForm";
             this.Text = "SearchForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GunsDGV)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button SearchBTN;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox TypeCMB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FirstNameTB;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox LastNameTB;
-        private System.Windows.Forms.Label LastNameLBL;
+        private System.Windows.Forms.Button SearchBTN;
+        private System.Windows.Forms.DataGridView GunsDGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Make;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Model;
     }
 }
--- MelsConsignment/SearchForm.cs
+++ MelsConsignment/SearchForm.cs
@@ -21,6 +21,9 @@ namespace MelsConsignment
 
         private void SearchBTN_Click(object sender, EventArgs e)
         {
+            Cursor.Current = Cursors.WaitCursor;
+
+            //Best place to have this code?
             //Establish connection and create collection
             MongoClient dbClient = new MongoClient("mongodb+srv://Mels105:MTP1946@cluster0.acccf.mongodb.net/test");
             var database = dbClient.GetDatabase("MelsTradingPost");
@@ -28,20 +31,43 @@ namespace MelsConsignment
 
             //Construct filter
             string lastName = LastNameTB.Text;
+            string firstName = FirstNameTB.Text;
+
             var filter = Builders<BsonDocument>.Filter.Eq("last_name", lastName);
+            var filter1 = Builders<BsonDocument>.Filter.Eq("first_name", firstName);
 
 
-
-            var consignmentDocument = collection.Find(filter).FirstOrDefault();
+            var consignmentDocument = collection.Find(filter & filter1).ToList();
+            
 
             if(consignmentDocument != null)
             {
-                lastName = consignmentDocument.GetValue("last_name").ToString();
+                int rowIndex = 0;
 
-                LastNameLBL.Text = lastName;
+                foreach(BsonDocument doc in consignmentDocument)
+                {
+                    Console.WriteLine(doc.GetValue("model").ToString());
+                    GunsDGV.Rows.Add(doc.GetValue("manufacturer").ToString(), doc.GetValue("model").ToString());
+                    rowIndex++;
+                }
+                
+                //Below code would be used when finding the first matching record.
+                //lastName = consignmentDocument.GetValue("last_name").ToString();
+                //string firstName = consignmentDocument.GetValue("first_name").ToString();
+
+                //LastTB.Text = lastName;
+                //FirstTB.Text = firstName;
+                //TypeTB.Text = consignmentDocument.GetValue("type").ToString();
+                //ManfTB.Text = consignmentDocument.GetValue("manufacturer").ToString();
+                //ModelTB.Text = consignmentDocument.GetValue("model").ToString();
+                //CalTB.Text = consignmentDocument.GetValue("chambering").ToString();
+                //PageTB.Text = consignmentDocument.GetValue("page").ToString();
             }
 
+            Cursor.Current = Cursors.WaitCursor;
 
         }
+
+
     }
 }
